7ae6a44499008b85c2c5c73e2f3bd0bcbe753e47
Daniel Stewart <stew0587@algonquinlive.com>
2023-07-21 19:26:04 -0400

    Ui layout done

--- app/src/main/java/algonquin/cst2335/cst2335_finalproject/BearImageMainActivity.java
+++ app/src/main/java/algonquin/cst2335/cst2335_finalproject/BearImageMainActivity.java
@@ -2,15 +2,33 @@ package algonquin.cst2335.cst2335_finalproject;
 
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class BearImageMainActivity extends AppCompatActivity {
+
+    TextView bearText;
+    EditText width;
+    EditText height;
+    Button generateButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bearimagegenerator_main);
+
+        bearText = findViewById(R.id.BearText);
+        width = findViewById(R.id.width);
+        height = findViewById(R.id.height);
+        generateButton = findViewById(R.id.generateButton);
+
+
 
 
     }
