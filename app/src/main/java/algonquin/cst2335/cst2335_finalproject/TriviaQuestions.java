6ed2e9bcdb5226763e627d9585d2ae9743104d10
andreroazk <andreroazk@gmail.com>
2023-07-21 19:42:46 -0400

    Squashed commit of the following:
    commit
    Project 1:
        - TriviaQuestions added
        - triviaquestion_main.xml added
        - dependencies added to build.gradle
    
    Author: andreroazk <andreroazk@gmail.com>
    Date:   Fri Jul 21 19:33:08 2023 -0400

---
+++ app/src/main/java/algonquin/cst2335/cst2335_finalproject/TriviaQuestions.java
@@ -0,0 +1,15 @@
+package algonquin.cst2335.cst2335_finalproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TriviaQuestions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.triviaquestion_main);
+    }
+}
