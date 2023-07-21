1f5e8c388881af611266975a89c933f03143d797
Daniel Stewart <stew0587@algonquinlive.com>
2023-07-21 13:08:37 -0400

    Commmit: Created Layout and Main Java Class for Bear Image Generator, Daniel Stewart

---
+++ app/src/main/java/algonquin/cst2335/cst2335_finalproject/BearImageMainActivity.java
@@ -0,0 +1,18 @@
+package algonquin.cst2335.cst2335_finalproject;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BearImageMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bearimagegenerator_main);
+
+
+    }
+
+}
