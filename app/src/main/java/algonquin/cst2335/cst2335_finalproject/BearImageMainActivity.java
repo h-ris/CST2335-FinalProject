b7f7056d0365342f3148d2d1e7f013fc00fba692
Daniel Stewart <stew0587@algonquinlive.com>
2023-07-26 15:43:19 -0400

    Commit

--- app/src/main/java/algonquin/cst2335/cst2335_finalproject/BearImageMainActivity.java
+++ app/src/main/java/algonquin/cst2335/cst2335_finalproject/BearImageMainActivity.java
@@ -2,8 +2,10 @@ package algonquin.cst2335.cst2335_finalproject;
 
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +25,8 @@ public class BearImageMainActivity extends AppCompatActivity {
     EditText height;
     Button generateButton;
 
+    Spinner helpMenu;
+
 
 
     @Override
@@ -30,8 +34,8 @@ public class BearImageMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bearimagegenerator_main);
 
-//        binding = ActivityBear
 
+        helpMenu = findViewById(R.id.helpMenu);
         bearText = findViewById(R.id.BearText);
         width = findViewById(R.id.width);
         height = findViewById(R.id.height);
@@ -39,6 +43,17 @@ public class BearImageMainActivity extends AppCompatActivity {
 
         String widthInput = width.getText().toString();
         String heightInput = height.getText().toString();
+
+
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this,
+                R.array.helparray, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        helpMenu.setAdapter(arrayAdapter);
+
+
+
 
         generateButton.setOnClickListener(clk -> {
             if(!widthInput.isEmpty() || !heightInput.isEmpty()){
